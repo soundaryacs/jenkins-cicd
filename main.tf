fbb0c1fc3f7ac15b83382623479428f124a71474
Soundarya CS <soundaryacs92@gmail.com>
2025-05-06 16:50:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-  ami = "ami-0f88e80871fd81e91"
-  instance_type = "t2.micro"
+  ami             = "ami-0f88e80871fd81e91"
+  instance_type   = "t2.micro"
   availability_zone = "us-east-1a"
+  
   tags = {
-    Name="JenkinsPractice"
+    Name = "JenkinsPractice"
   }
+}
