44c33ec931dc0d6d32b5d9764a68e9e598df62d5
Soundarya CS <soundaryacs92@gmail.com>
2025-05-07 15:03:14 +0530

    Update main.tf

--- Day-01-DIR/main.tf
+++ Day-01-DIR/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-  ami             = "ami-0f88e80871fd81e91"
+  ami             = "ami-084568db4383264d4"
   instance_type   = "t2.micro"
   availability_zone = "us-east-1a"
   
