7d4f3737b3643f0beaea5e938c0eb47b48689d44
Soundarya CS <soundaryacs92@gmail.com>
2025-05-07 15:23:30 +0530

    Jenkins Build after projects

---
+++ triggers/test/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+  ami             = "ami-084568db4383264d4"
+  instance_type   = "t2.micro"
+  availability_zone = "us-east-1a"
+  
+  tags = {
+    Name = "Jenkins Trigger Test"
+  }
+}
